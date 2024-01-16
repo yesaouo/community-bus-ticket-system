a1606bb21151186e288d9a9396ae7bb62138e3fb
yesaouo <ethan147852369@gmail.com>
2024-01-16 22:02:29 +0800

    add merge func

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+import { getAuth, onAuthStateChanged, signInWithPopup, GoogleAuthProvider, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +21,83 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth();
+const db = getFirestore(app);
 
+async function setTicket(id) {
+    const localId = localStorage.getItem('CBTS-Ticket');
+    if (localId && localId === id) {
+        const localPoint = await getTicket(localId);
+        if (localPoint) {
+            setTicketDiv(localId, localPoint);
+        }
+    } else {
+        const localPoint = await getTicket(localId);
+        const cloudPoint = await getTicket(id);
+        if (localPoint && cloudPoint) {
+            mergeTextDiv.innerHTML = `本地餘額: ${localPoint}<br>帳戶餘額: ${cloudPoint}`;
+            unmergeBtn.innerHTML = '取消登入';
+            synopsisDiv.style.display = 'none';
+            ticketDiv.style.display = 'none';
+            mergeDiv.style.display = 'block';
+
+            unmergeBtn.onclick = () => {
+                auth.signOut();
+                alert('已登出');
+            }
+            mergeBtn.onclick = () => {
+                if (confirm('')) {
+                    //下載原本車票並變更 合併給管理員
+                }
+            }
+        } else if (localPoint) {
+            setTicketDiv(localId, localPoint);
+        } else if (cloudPoint) {
+            setTicketDiv(id, cloudPoint);
+        }
+    }
+
+    async function getTicket(id) {
+        if (id && typeof id === 'string' && id.length === 20) {
+            const docSnap = await getDoc(doc(db, "tickets", id));
+            if (docSnap.exists() && docSnap.data().point)
+                return docSnap.data().point;
+        }
+        return null;
+    }
+    function setTicketDiv(id, point) {
+        qrcode.makeCode(`CBTS${id}`);
+        pointSpan.innerHTML = point;
+        refreshBtn.innerHTML = '<img src="img/refresh.png">';
+        synopsisDiv.style.display = 'none';
+        mergeDiv.style.display = 'none';
+        ticketDiv.style.display = 'block';
+    }
+}
+onAuthStateChanged(auth, async(user) => {
+    if (user) {
+        userBtn.innerHTML = `<img src="${user.photoURL}">`;
+        const docSnap = await getDoc(doc(db, "users", user.uid));
+        if (docSnap.exists() && docSnap.data().ticket)
+            setTicket(docSnap.data().ticket);
+
+        userBtn.onclick = () => {
+            if (confirm('確定要登出嗎'))
+                auth.signOut();
+        }
+    } else {
+        userBtn.innerHTML = '<img src="img/profile-user.png">';
+        if (localStorage.getItem('CBTS-Ticket'))
+            setTicket(localStorage.getItem('CBTS-Ticket'));
+
+        userBtn.onclick = () => {
+            if (confirm('將跳轉至Google進行第三方登入\n如果未能自動跳轉，請確保您開啟了"彈出式視窗與重新導向"的權限。')) {
+                const provider = new GoogleAuthProvider();
+                signInWithPopup(auth, provider);
+            }
+        }
+    }
+});
 
 ////////////////////////////////
 
